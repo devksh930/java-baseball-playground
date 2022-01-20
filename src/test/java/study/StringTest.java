1b414e6dc6774ce188fc5fd58e6c7a3322d259e2
devksh930 <ksh930@icloud.com>
2022-01-20 21:03:22 +0900

    init

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    @DisplayName("replace 테스트")
+    void replace() {
+        String actual = "abc".replace("b", "d");
+        assertThat(actual).isEqualTo("adc");
+        
+    }
+}
