48a65154ce442ec1bd27d22cc627854c0f42465b
devksh930 <ksh930@icloud.com>
2022-01-20 21:18:46 +0900

    split test구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,6 +11,22 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
-        
     }
+
+    @Test
+    @DisplayName("1,2를 , 로 split 한다")
+    void splitTest1() {
+        String text = "1,2";
+        String[] split = text.split(",");
+        assertThat(split).contains("1").contains("2");
+    }
+
+    @Test
+    @DisplayName("1을 , 로 spliat 한다")
+    void splitTest2() {
+        String text = "1";
+        String[] split = text.split(",");
+        assertThat(split).containsExactly("1");
+    }
+
 }
