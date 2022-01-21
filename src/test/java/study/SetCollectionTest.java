0a99afc4989dd6621184a687c0df3946421503f3
devksh930 <ksh930@icloud.com>
2022-01-21 22:25:22 +0900

    Set contains 테스트 리팩토링

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -3,11 +3,15 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class SetCollectionTest {
     private Set<Integer> numbers;
@@ -36,6 +40,13 @@ public class SetCollectionTest {
         assertThat(numbers.contains(3)).isTrue();
         assertThat(numbers.contains(3)).isTrue();
         assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @DisplayName("Set에 값이 존재하는지 확인한다. 리팩토링")
+    void contains_refactor(int number) {
+        assertTrue(numbers.contains(number));
     }
 
 }
